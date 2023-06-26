d47ac9483aef50aa5e59735c17715ea11c4d6476
kh1012 <kh1012@midasit.com>
2023-06-26 17:23:48 +0900

    tailwind css 추가 및 테스트 페이지 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import JumoButton from './components/JumoButton/JumoButton';
+import React from 'react';
+import { VerifyDialog, VerifyUtil } from "./exports/index";
+import "./output.css";
 
-const App = () => (
-  <JumoButton backgroundColor="blue" fontColor="white">
-    테스트 버튼
-  </JumoButton>
-);
+const App = () => {
+	const [ productionUrl, setProductionUrl ] = React.useState("");
+	const [ baseUrl, setBaseUrl ] = React.useState("");
+	const [ mapiKey, setMapiKey ] = React.useState("");
+
+	React.useEffect(() => {
+		const initAsync = async () => {
+			setProductionUrl(VerifyUtil.getProductionUrl());
+			setBaseUrl(await VerifyUtil.getBaseUrlAsync());
+			setMapiKey(VerifyUtil.getMapiKey());
+		}
+		initAsync();
+	}, []);
+
+	return (
+		<>
+			<VerifyDialog />
+			<div className='text-blue-500'>PRODUCTION URL : {productionUrl}</div>
+			<div className='myClass2'>BASEURL : {baseUrl}</div>
+			<div className='myClass3'>MAPIKEY : {mapiKey}</div>
+		</>
+	)
+}
 
 export default App;
